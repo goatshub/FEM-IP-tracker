b319865e2e27ba6313c1c21962eaa295974ae578
goatshub <kiatwongwanich@gmail.com>
2023-06-21 09:42:49 +0700

    remove comment

--- src/types/ipContext.ts
+++ src/types/ipContext.ts
@@ -9,17 +9,6 @@ export type IpDataType = {
   ip: string;
   [key: string]: any;
 };
-// export type IpDataType = {
-//   message?: string;
-//   lat: number;
-//   lon: number;
-//   isp: string;
-//   timezone: string;
-//   city: string;
-//   regionName: string;
-//   query: string;
-//   [key: string]: any;
-// };
 
 export type IpContextType = {
   IpData: IpDataType | undefined;
